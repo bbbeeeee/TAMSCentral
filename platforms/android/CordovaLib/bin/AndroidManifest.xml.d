4f091d531f1d8fb093e09da0fa87f266efde8c31
Brandon Truong <me@brandontruong.com>
2014-05-19 23:43:42 -0500

    First commit

---
+++ platforms/android/CordovaLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/brandon/dev/TAMSCentral/platforms/android/CordovaLib/bin/AndroidManifest.xml : \
+/home/brandon/dev/TAMSCentral/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/CordovaLib/gen/R.java.d
@@ -0,0 +1,1 @@
+ : /home/brandon/dev/TAMSCentral/platforms/android/CordovaLib/bin/AndroidManifest.xml \
